6c9822e23c7d9a98beec099fc5be246efcca7650
Gerd Reiss <gerdreiss@@users.noreply.github.com>
2022-09-03 11:10:49 +0200

    [Rock the JVM Course Spark Essentials] Spark SQL

--- src/main/scala/lessons/part4sql/SparkSql.scala
+++ src/main/scala/lessons/part4sql/SparkSql.scala
@@ -1,15 +1,20 @@
 package lessons.part4sql
 
+import commons.Utils._
+import org.apache.commons.io.FileUtils
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{ DataFrame, SaveMode, SparkSession }
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+
+import java.io.File
 
 object SparkSql extends App {
 
+  val warehouseDir = "src/main/resources/warehouse"
   val spark = SparkSession
     .builder()
     .appName("Spark SQL Practice")
     .config("spark.master", "local")
-    .config("spark.sql.warehouse.dir", "src/main/resources/warehouse")
+    .config("spark.sql.warehouse.dir", warehouseDir)
     // only for Spark 2.4 users:
     // .config("spark.sql.legacy.allowCreatingManagedTableUsingNonemptyLocation", "true")
     .getOrCreate()
@@ -29,7 +34,7 @@ object SparkSql extends App {
   // we can run ANY SQL statement
   spark.sql("create database rtjvm")
   spark.sql("use rtjvm")
-  spark.sql("show databases")
+  spark.sql("show databases").show(false)
 
   // transfer tables from a DB to Spark tables
   val driver   = "org.postgresql.Driver"
@@ -37,18 +42,30 @@ object SparkSql extends App {
   val user     = "docker"
   val password = "docker"
 
-  def readTable(tableName: String): DataFrame = spark.read
-    .format("jdbc")
-    .option("driver", driver)
-    .option("url", url)
-    .option("user", user)
-    .option("password", password)
-    .option("dbtable", s"public.$tableName")
-    .load()
+  def readTable(tableName: String): DataFrame =
+    spark.read
+      .format("jdbc")
+      .option("driver", driver)
+      .option("url", url)
+      .option("user", user)
+      .option("password", password)
+      .option("dbtable", s"public.$tableName")
+      .load()
+
+  def readTable2(tableName: String): DataFrame =
+    spark.read.jdbc(
+      url,
+      s"public.$tableName",
+      Map(
+        "driver"   -> driver,
+        "user"     -> user,
+        "password" -> password,
+      ).toJavaProperties,
+    )
 
   def transferTables(tableNames: List[String], shouldWriteToWarehouse: Boolean = false): Unit =
     tableNames.foreach { tableName =>
-      val tableDF = readTable(tableName)
+      val tableDF = readTable2(tableName)
       tableDF.createOrReplaceTempView(tableName)
 
       if (shouldWriteToWarehouse) {
@@ -61,7 +78,9 @@ object SparkSql extends App {
   transferTables(List("employees", "departments", "titles", "dept_emp", "salaries", "dept_manager"))
 
   // read DF from loaded Spark tables
-  spark.read.table("employees")
+  spark.read.table("employees").show(false)
+  spark.read.table("departments").show(false)
+  spark.read.table("titles").show(false)
 
   /**
     * Exercises
@@ -78,22 +97,27 @@ object SparkSql extends App {
     .json("src/main/resources/data/movies.json")
     .cache()
 
+  FileUtils.deleteDirectory(new File(warehouseDir))
+
   moviesDF.write
     .mode(SaveMode.Overwrite)
     .saveAsTable("movies")
 
   // 2
-  spark.sql(
-    """
+  spark
+    .sql(
+      """
       |select count(*)
       |  from employees
       | where hire_date > '1999-01-01' and hire_date < '2000-01-01'
     """.stripMargin
-  )
+    )
+    .show(false)
 
   // 3
-  spark.sql(
-    """
+  spark
+    .sql(
+      """
       |select de.dept_no, avg(s.salary)
       |  from employees e, dept_emp de, salaries s
       | where e.hire_date > '1999-01-01' and e.hire_date < '2000-01-01'
@@ -101,7 +125,8 @@ object SparkSql extends App {
       |   and e.emp_no = s.emp_no
       | group by de.dept_no
     """.stripMargin
-  )
+    )
+    .show(false)
 
   // 4
   spark
@@ -118,5 +143,5 @@ object SparkSql extends App {
       | limit 1
     """.stripMargin
     )
-    .show()
+    .show(false)
 }
