f209fb26e7806d413f5e18c02a58b7fd005b04f8
Gerd Reiss <gerdreiss@@users.noreply.github.com>
2022-09-05 09:55:39 +0200

    [Rock the JVM Course Spark Essentials] deploying

--- src/main/scala/lessons/part6practical/TestDeployApp.scala
+++ src/main/scala/lessons/part6practical/TestDeployApp.scala
@@ -1,12 +1,12 @@
 package lessons.part6practical
 
-import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
-
+import org.apache.spark.sql.{ SaveMode, SparkSession }
 
 object TestDeployApp {
 
   def main(args: Array[String]): Unit = {
+
     /**
       * Movies.json as args(0)
       * GoodComedies.json as args(1)
@@ -19,23 +19,29 @@ object TestDeployApp {
       System.exit(1)
     }
 
-    val spark = SparkSession.builder()
+    val spark = SparkSession
+      .builder()
       .appName("Test Deploy App")
       .getOrCreate()
 
     val moviesDF = spark.read
       .option("inferSchema", "true")
       .json(args(0))
+      .cache()
 
-    val goodComediesDF = moviesDF.select(
-      col("Title"),
-      col("IMDB_Rating").as("Rating"),
-      col("Release_Date").as("Release")
-    )
-      .where(col("Major_Genre") === "Comedy" and col("IMDB_Rating") > 6.5)
+    val goodComediesDF = moviesDF
+      .select(
+        col("Title"),
+        col("IMDB_Rating").as("Rating"),
+        col("Release_Date").as("Release"),
+      )
+      .where(
+        col("Major_Genre") === "Comedy" and
+          col("IMDB_Rating") > 6.5
+      )
       .orderBy(col("Rating").desc_nulls_last)
 
-    goodComediesDF.show
+    goodComediesDF.show(false)
 
     goodComediesDF.write
       .mode(SaveMode.Overwrite)
@@ -43,19 +49,19 @@ object TestDeployApp {
       .save(args(1))
   }
 
-   /*
-    * Build a JAR to run a Spark application on the Docker cluster
-    *
-    *   - project structure -> artifacts, add artifact from "module with dependencies"
-    *   - (important) check "copy to the output folder and link to manifest"
-    *   - (important) then from the generated folder path, delete so that the folder path ends in src/
-    *
-    * Build the JAR: Build -> Build Artifacts... -> select the jar -> build
-    * Copy the JAR and movies.json to spark-cluster/apps
-    * (the apps and data folders are mapped to /opt/spark-apps and /opt/spark-data in the containers)
-    *
-    *
-    * */
+  /*
+   * Build a JAR to run a Spark application on the Docker cluster
+   *
+   *   - project structure -> artifacts, add artifact from "module with dependencies"
+   *   - (important) check "copy to the output folder and link to manifest"
+   *   - (important) then from the generated folder path, delete so that the folder path ends in src/
+   *
+   * Build the JAR: Build -> Build Artifacts... -> select the jar -> build
+   * Copy the JAR and movies.json to spark-cluster/apps
+   * (the apps and data folders are mapped to /opt/spark-apps and /opt/spark-data in the containers)
+   *
+   *
+   * */
 
   /**
     * How to run the Spark application on the Docker cluster
--- src/main/scala/lessons/part7bigdata/TaxiEconomicImpact.scala
+++ src/main/scala/lessons/part7bigdata/TaxiEconomicImpact.scala
@@ -1,14 +1,19 @@
 package lessons.part7bigdata
 
-import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.{ SaveMode, SparkSession }
 
 object TaxiEconomicImpact {
 
   def main(args: Array[String]): Unit = {
 
     if (args.length != 3) {
-      println("Need 1) big data source, 2) taxi zones data source, 3) output data destination")
+      println("""
+          |Need:
+          | 1) big data source,
+          | 2) taxi zones data source,
+          | 3) output data destination
+          |""".stripMargin)
       System.exit(1)
     }
 
@@ -17,13 +22,15 @@ object TaxiEconomicImpact {
       2 - taxi zones data source
       3 - output data destination
      */
-    val spark = SparkSession.builder()
+    val spark = SparkSession
+      .builder()
       .config("spark.master", "local")
       .appName("Taxi Big Data Application")
       .getOrCreate()
+
     import spark.implicits._
 
-    val bigTaxiDF = spark.read.load(args(0))
+    val bigTaxiDF   = spark.read.load(args(0)).cache()
     val taxiZonesDF = spark.read
       .option("header", "true")
       .option("inferSchema", "true")
@@ -31,15 +38,19 @@ object TaxiEconomicImpact {
 
     bigTaxiDF.printSchema()
 
-    val percentGroupAttempt = 0.05
+    val percentGroupAttempt   = 0.05
     val percentAcceptGrouping = 0.3
-    val discount = 5
-    val extraCost = 2
-    val avgCostReduction = 0.6 * bigTaxiDF.select(avg(col("total_amount"))).as[Double].take(1)(0)
-    val percentGroupable = 289623 * 1.0 / 331893
+    val discount              = 5
+    val extraCost             = 2
+    val avgCostReduction      = 0.6 * bigTaxiDF.select(avg(col("total_amount"))).as[Double].take(1)(0)
+    val percentGroupable      = 289623 * 1.0 / 331893
 
     val groupAttemptsDF = bigTaxiDF
-      .select(round(unix_timestamp(col("pickup_datetime")) / 300).cast("integer").as("fiveMinId"), col("pickup_taxizone_id"), col("total_amount"))
+      .select(
+        round(unix_timestamp(col("pickup_datetime")) / 300).cast("integer").as("fiveMinId"),
+        col("pickup_taxizone_id"),
+        col("total_amount"),
+      )
       .groupBy(col("fiveMinId"), col("pickup_taxizone_id"))
       .agg((count("*") * percentGroupable).as("total_trips"), sum(col("total_amount")).as("total_amount"))
       .orderBy(col("total_trips").desc_nulls_last)
@@ -50,7 +61,10 @@ object TaxiEconomicImpact {
 
     val groupingEstimateEconomicImpactDF = groupAttemptsDF
       .withColumn("groupedRides", col("total_trips") * percentGroupAttempt)
-      .withColumn("acceptedGroupedRidesEconomicImpact", col("groupedRides") * percentAcceptGrouping * (avgCostReduction - discount))
+      .withColumn(
+        "acceptedGroupedRidesEconomicImpact",
+        col("groupedRides") * percentAcceptGrouping * (avgCostReduction - discount),
+      )
       .withColumn("rejectedGroupedRidesEconomicImpact", col("groupedRides") * (1 - percentAcceptGrouping) * extraCost)
       .withColumn("totalImpact", col("acceptedGroupedRidesEconomicImpact") + col("rejectedGroupedRidesEconomicImpact"))
 
